c906702f5dac6eec20753a9de36b4b214e45e87c
Nimzyow <n_soufiani@hotmail.com>
2020-02-22 23:12:41 +0000

    airport challenge done

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,17 @@
 class Airport
+  DEFAULT_CAPACITY = 4
+
   attr_accessor :hanger, :capacity
   attr_reader :name
-
-  def initialize(name:, capacity: 4)
+  
+  def initialize(name:, capacity: DEFAULT_CAPACITY)
     @hanger = []
     @name = name
     @capacity = capacity
   end
 
   def land_plane(plane)
+    raise "Stormy, you can't land...go to a non stormy country" if is_stormy?
     raise "cannot land plane, hanger full" if is_full?
     store_plane(plane)
     
@@ -19,13 +22,23 @@ class Airport
   end
 
   def take_off_plane(flight_number)
-    @hanger.map.with_index {
-      |plane, i|
+    raise "it's stormy. Have a sit in the airport lounge until the storm clears off" if is_stormy?
+    @hanger.map.with_index { |plane, i|
       if plane[:flight_number] == flight_number
+        takeoff_message(plane)
         return @hanger.delete_at(i) 
       end
     }
+    exceptions
+  end
+
+  def exceptions
+    raise "plane does not exist in the hanger to take off" if @hanger.length > 0
     raise "no planes to take off"
+  end
+
+  def takeoff_message(plane)
+    puts "Welcome on board to #{plane[:airline]}. We are about to take off to #{plane[:next_takeoff_destination]}. We hope you have a pleasant journey and thank you for choosing #{plane[:airline]}"
   end
 
   def is_stormy?
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -5,6 +5,8 @@ shared_context "common airport" do
     let(:default_capacity) {4}
     let(:plane_klass) {instance_double(Plane, {flight_details: {airline: "Qatar Airways", flight_number: "QA101", next_takeoff_destination:"Tokyo"}})}
     let(:flight_details) {plane_klass.flight_details}
+    let(:no_storm) { allow(airport).to receive(:is_stormy?).and_return(false)}
+    let(:stormy) {allow(airport).to receive(:is_stormy?).and_return(true)}
 end
 
 describe Airport do
@@ -42,55 +44,85 @@ describe Airport do
   describe "method functionality - " do
     context "hanger state change" do
       it "store_plane should push plane into hanger array" do
+        no_storm
         expect{airport.store_plane(flight_details)}.to change{airport.hanger.length}.by(1) 
       end
 
       it "hanger array includes 1 flight detail" do
+        no_storm
         airport.land_plane(flight_details)
         expect(airport.hanger).to include(flight_details)
       end
+
+      it "#is_stormy should return true or false" do
+        expect(airport.is_stormy?).to eq(true).or eq(false)
+      end
     end
     context "#take_off_plane" do
-    it "#take_off_plane should remove plane from hanger array" do
-      flight_number_to_take_off = "TH101"
-      airport.land_plane(flight_details)
-      expect{airport.take_off_plane("QA101")}.to change{airport.hanger.length}.by(-1)
-    end
-
-    it "take_off_plane will no longer have the plane in hanger" do
-      airport.land_plane(flight_details)
-      airport.take_off_plane("QA101")
-      expect(airport.hanger).to_not include(flight_details)
-    end
-
-    it "land 3 planes and only allow the 2nd plane to take off" do
-      def generate_flight(airline:, flight_number:, next_takeoff_destination:)
-        plane = plane_klass
-        allow(plane).to receive(:flight_details).and_return({airline: airline, flight_number: flight_number, next_takeoff_destination: next_takeoff_destination})
-        return plane
+      it "should remove plane from hanger array" do
+        no_storm
+        flight_number_to_take_off = "TH101"
+        airport.land_plane(flight_details)
+        expect{airport.take_off_plane("QA101")}.to change{airport.hanger.length}.by(-1)
       end
-      
-      plane_one = flight_details
-      airport.land_plane(plane_one)
 
-      plane_two = generate_flight({airline: "Thai Airways", flight_number: "TH101", next_takeoff_destination:"Bangkok"})
-      airport.land_plane(plane_two.flight_details)
+      it "will no longer have the plane in hanger" do
+        no_storm
+        airport.land_plane(flight_details)
+        airport.take_off_plane("QA101")
+        expect(airport.hanger).to_not include(flight_details)
+      end
 
-      plane_three = generate_flight({airline: "Etihad Airways", flight_number: "ET101", next_takeoff_destination:"Dubai"})
-      airport.land_plane(plane_three.flight_details)
-      
-      airport.take_off_plane("TH101")
-      expect(airport.hanger).not_to include({airline: "Thai Airways", flight_number: "TH101", next_takeoff_destination:"Bangkok"})
+      it "land 3 planes and only allow the 2nd plane to take off" do
+        no_storm
+        def generate_flight(airline:, flight_number:, next_takeoff_destination:)
+          plane = plane_klass
+          allow(plane).to receive(:flight_details).and_return({airline: airline, flight_number: flight_number, next_takeoff_destination: next_takeoff_destination})
+          return plane
+        end
+        
+        plane_one = flight_details
+        airport.land_plane(plane_one)
+
+        plane_two = generate_flight({airline: "Thai Airways", flight_number: "TH101", next_takeoff_destination:"Bangkok"})
+        airport.land_plane(plane_two.flight_details)
+
+        plane_three = generate_flight({airline: "Etihad Airways", flight_number: "ET101", next_takeoff_destination:"Dubai"})
+        airport.land_plane(plane_three.flight_details)
+        
+        airport.take_off_plane("TH101")
+        expect(airport.hanger).not_to include({airline: "Thai Airways", flight_number: "TH101", next_takeoff_destination:"Bangkok"})
+      end
     end
   end
-    it "prevent land_plane if airport capacity is full" do
+  context "raise_error functionality - " do
+    it "prevent #land_plane if airport capacity is full" do
+      no_storm
       allow(airport).to receive(:is_full?).and_return(true)
       expect{airport.land_plane(flight_details)}.to raise_error("cannot land plane, hanger full")
     end
 
-    it "is_stormy should return true or false" do
-      expect(airport.is_stormy?).to eq(true).or eq(false)
+    it "prevent take off if stormy and raises error" do
+      no_storm
+      airport.land_plane(flight_details)
+      stormy
+      expect{airport.take_off_plane("QA101")}.to raise_error("it's stormy. Have a sit in the airport lounge until the storm clears off")
+    end
+
+    it "raises error when planes are in the hanger but user attempts to take off a plane that is not included in the hanger" do
+      no_storm
+      airport.land_plane(flight_details)
+      expect{airport.take_off_plane("JAL101")}.to raise_error("plane does not exist in the hanger to take off")
+    end
+
+    it "raises error when no planes are in the hanger" do
+      no_storm
+      expect{airport.take_off_plane("TH101")}.to raise_error("no planes to take off")
+    end
+
+    it "#land_plane prevents landing if stormy and raises error" do
+      stormy
+      expect{airport.land_plane(flight_details)}.to raise_error("Stormy, you can't land...go to a non stormy country")
     end
   end
-
 end
